4bc9ec425e698d49f96ef5f71b278dfaebca2b76
Alex <alex5402private@gmail.com>
2024-04-23 09:25:18 +0530

    improved

--- build.sh
+++ build.sh
@@ -36,7 +36,7 @@ function zipping()
 git clone --depth=1 https://github.com/sarthakroy2002/AnyKernel3.git AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
-zip -r9 OSS-KERNEL-RMX2020-NEOLIT-KSU.zip *
+zip -r9 OSS-KERNEL-RMX2020-NEOLIT.zip *
 }
 
 compile
