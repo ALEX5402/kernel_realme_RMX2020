bd231a08fe4d3aa7148813ed931bab8ac6340c3b
Alex <alex5402private@gmail.com>
2024-04-25 13:37:41 +0000

    RMX2020-kerne: added KSU
    B
    
    Signed-off-by: Alex <alex5402private@gmail.com>

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ rm -rf AnyKernel
 git clone --depth=1 https://github.com/alex5402/AnyKernel3.git AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
-zip -r9 Test-OSS-KERNEL-RMX2020-NEOLIT-KSU.zip *
+zip -r9 OSS-KERNEL-RMX2020-NEOLIT-KSU.zip *
 }
 
 compile
