ad57c35e952cadb3e5f306bf1b2cbb664d0df8d2
Alex <alex5402private@gmail.com>
2024-04-25 15:07:03 +0000

    RMX2020-kerne: added KSU
    B
    
    Signed-off-by: Alex <alex5402private@gmail.com>

--- build.sh
+++ build.sh
@@ -13,7 +13,8 @@ git clone --depth=1 https://gitlab.com/sarthakroy2002/android_prebuilts_clang_ho
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
 
-make O=out ARCH=arm64 RMX2020_defconfig
+make O=out ARCH=arm64 RMX2020_defconfig clean
+# make O=out ARCH=arm64 RMX2020_defconfig rmx2020-stock_defconfig
 
 PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
 make -j$(nproc --all) O=out \
